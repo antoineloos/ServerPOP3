785e3472e801fa38829e9f79769295223adc6ce7
Iguane42 <bocquet.yoann@gmail.com>
2017-02-08 09:19:12 +0100

    Ajout de SSL

--- src/serveurpop3/Serveur.java
+++ src/serveurpop3/Serveur.java
@@ -6,10 +6,14 @@
 package serveurpop3;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 
 /**
  *
@@ -17,17 +21,22 @@ import java.util.logging.Logger;
  */
 public class Serveur implements Runnable {
 
-    private ServerSocket socketServeur;
-    private Socket socket;
+    private SSLServerSocket socketServeur;
+    private SSLSocket socket;
     private Thread thread;
 
     public Serveur(int port) {
         try {
-
-            socketServeur = new ServerSocket(port);
+            SSLServerSocketFactory factory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+            socketServeur = (SSLServerSocket)factory.createServerSocket(port);
+            socketServeur.setEnabledCipherSuites(
+                    Arrays.stream(factory.getSupportedCipherSuites())
+                        .filter(s -> s.contains("anon"))
+                        .toArray(size -> new String[size])
+            );
             Utilitaire.print("\n Le serveur écoute sur le port " + socketServeur.getLocalPort(), Utilitaire.ANSI_GREEN);
 
-        } catch (IOException ex) {//Erreur création du serverSocket
+        } catch (Exception ex) {//Erreur création du serverSocket
             Utilitaire.print("\n Erreur à la création du serverSocket ", Utilitaire.ANSI_RED);
             Logger.getLogger(Serveur.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -37,7 +46,7 @@ public class Serveur implements Runnable {
     public void run() {
         while (true) {
             try {
-                socket = socketServeur.accept();
+                socket = (SSLSocket)socketServeur.accept();
                 thread = new Thread(new Communication(socket));
                 thread.start();
             } catch (IOException ex) {//Erreur du accept
